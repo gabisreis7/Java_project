a9002823d296c43fce6a69e6231f8233784a5440
gabisreis7 <gabisousareis07@gmail.com>
2024-10-16 15:28:13 -0300

    Aplicação de regra de negócios

--- src/main/java/org/example/service/ManutencaoServiceImpl.java
+++ src/main/java/org/example/service/ManutencaoServiceImpl.java
@@ -1,10 +1,33 @@
 package org.example.service;
 
 import org.example.dao.ManutencaoDao;
+import org.example.entities.Manutencao;
 
-public class ManutencaoServiceImpl {
+import java.util.List;
+
+public class ManutencaoServiceImpl implements ManutencaoService{
 
 
     public ManutencaoServiceImpl(ManutencaoDao manutencaoDao) {
+    }
+
+    @Override
+    public void agendarManutencao(Manutencao manutencao) {
+
+    }
+
+    @Override
+    public List<Manutencao> listarManutencoes() {
+        return List.of();
+    }
+
+    @Override
+    public void atualizarManutencao(Manutencao manutencao) {
+
+    }
+
+    @Override
+    public void deletarManutencao(int id) {
+
     }
 }
